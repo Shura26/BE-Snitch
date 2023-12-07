d16c0c6fde34ece0f945137e8840cb80c41d2be2
Dulan Jayalatharachchi <51435815+Shura26@users.noreply.github.com>
2023-12-07 11:43:56 +0100

    add scrap new anime & ep

--- main.py
+++ main.py
@@ -76,6 +76,77 @@ def extractPseudos(html):
         print("Balises non trouvées")
 
 
+def scrapNewLink():
+    url = "https://www.crunchyroll.com/fr/videos/new"
+
+    # Configuration de Selenium
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1200x600')
+
+    if "crunchyroll" in url:
+        driver = uc.Chrome()
+        driver.get(url)
+
+        wait = WebDriverWait(driver, 2)
+
+        # Scrolle
+        nbScrolle = range(5)
+        for scrolle in nbScrolle:
+            driver.find_element(By.XPATH, '//body').send_keys(Keys.PAGE_DOWN)
+            time.sleep(0.5)  # temps d'attente entre les défilements
+            # check si fin de page atteint
+            end_of_page = driver.execute_script(
+                "return window.innerHeight + window.scrollY >= document.body.scrollHeight;")
+            if end_of_page:
+                break
+
+        html = driver.page_source
+        driver.quit()
+
+        soup = BeautifulSoup(html, 'html.parser')
+        contenuInClass = soup.find_all('a', class_='browse-card__poster-wrapper--pU-AW')
+
+    if contenuInClass:
+        with open('AnimeLinks.txt', 'a', encoding='utf-8') as fichier:
+            for link in contenuInClass:
+                contenu = link.get('href')
+                fichier.write("https://www.crunchyroll.com" + contenu + '\n')
+        print("Liens sauvegardés dans AnimeLink.txt.")
+
+    else:
+        print("Balises non trouvées")
+
+
+
+def scrapEpLink(url):
+    # Configuration de Selenium
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1200x600')
+
+    if "crunchyroll" in url:
+        driver = uc.Chrome()
+        driver.get(url)
+
+        wait = WebDriverWait(driver, 10)
+        scroll_to_bottom(driver)
+        html = driver.page_source
+        driver.quit()
+
+        soup = BeautifulSoup(html, 'html.parser')
+        contenu_in_class = soup.find_all('a', class_='playable-card__thumbnail-wrapper--BkWZo')
+
+        if contenu_in_class:
+            with open('EpLinks.txt', 'a', encoding='utf-8') as fichier:
+                for link in contenu_in_class:
+                    contenu = link.get('href')
+                    fichier.write("https://www.crunchyroll.com" + contenu + '\n')
+            print("Liens sauvegardés dans EpLink.txt.")
+        else:
+            print("Balises non trouvées")
 
 
 #sur ADN,après le nom de l'anime dans l'url les ep sont numéroter ex:ep1 de fairy tail => https://animationdigitalnetwork.fr/video/fairy-tail/3628
@@ -83,5 +154,9 @@ def extractPseudos(html):
  #   url = 'https://animationdigitalnetwork.fr/video/fairy-tail/' + str(epID)
   #  scrape(url)
 
-url = 'https://www.crunchyroll.com/fr/watch/G69XGG44R/ryomen-sukuna'
-scrape(url)
+#url = 'https://www.crunchyroll.com/fr/watch/G69XGG44R/ryomen-sukuna'
+#scrape(url)
+scrapNewLink()
+with open('AnimeLinks.txt', 'r', encoding='utf-8') as fichierAnimeList:
+    for ligne in fichierAnimeList:
+        scrapEpLink(ligne)
