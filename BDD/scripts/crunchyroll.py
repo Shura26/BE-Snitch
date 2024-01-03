311fbf68ff8200fb1d116665fd0a013ac0e2ab67
Dulan Jayalatharachchi <51435815+Shura26@users.noreply.github.com>
2024-01-03 18:03:41 +0100

    Delete BDD directory

--- BDD/scripts/crunchyroll.py
+++
@@ -1,6 +0,0 @@
-#ajoute un nouveau tableau de pseudos dans la table concernée (chaque table a pour nom l'id de l'anime)
-def update_pseudos(table, users):
-    pass
-
-def get_ids_already_checked():
-    pass
